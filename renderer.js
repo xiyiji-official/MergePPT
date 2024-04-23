769b9c53e1426e83f581782cdb921cce5df38d6d
xiyiji <wujiaming@tsinghauvc.com.cn>
2024-04-23 18:52:51 +0800

    :fix: 修改和优化了拖拽中获取参数和定位目标的逻辑

--- renderer.js
+++ renderer.js
@@ -4,78 +4,81 @@ const divList = document.getElementById("list");
 
 // 立即执行 初始化渲染中间的文件列表
 (async function () {
-  const selectType =
-    document.getElementById("select").options[
-      document.getElementById("select").selectedIndex
-    ].value;
-  divList.innerHTML = await window.electronAPI.initList(selectType);
+    const selectType =
+        document.getElementById("select").options[
+            document.getElementById("select").selectedIndex
+            ].value;
+    divList.innerHTML = await window.electronAPI.initList(selectType);
 })();
 
 // 监听刷新按钮点击事件，执行更新文件列表功能
 freshBtn.addEventListener("click", async () => {
-  const selectType =
-    document.getElementById("select").options[
-      document.getElementById("select").selectedIndex
-    ].value;
-  divList.innerHTML = await window.electronAPI.updateList(selectType);
+    const selectType =
+        document.getElementById("select").options[
+            document.getElementById("select").selectedIndex
+            ].value;
+    divList.innerHTML = await window.electronAPI.updateList(selectType);
 });
 
 
 divList.addEventListener("dragstart", (event) => {
-  let targeElement = event.target.id;
-  console.log(targeElement)
-  event.dataTransfer.setData("text", targeElement);
+    let targeElement = event.target.id;
+    console.log(targeElement)
+    event.dataTransfer.setData("startID", targeElement);
 });
 
 // 监听divList上的【拖动到】事件
-var mudi1;
+var lastTargetID; // 设定一个变量用以接收ID的变化
 divList.addEventListener("dragover", (event) => {
-  event.preventDefault();
-  event.stopPropagation()
-  var mudi2 = event.target.id;
-  if (mudi2 === "") {
-  } else {
-    if (mudi1 === mudi2) {
-      // console.log(`依旧留存在同一个div中`);
+    event.preventDefault();
+    event.stopPropagation()
+    var targetID = event.target.id; // 目前停留在的目标元素ID
+
+    if (targetID === "") {
+        console.log(`为空：target ID: ${targetID}`); // 判断一下这个ID是不是存在
     } else {
-      console.log(mudi1)
-      if (mudi1 === undefined) {
-        // console.log(`已经离开前一个div，前一个${mudi1},后一个${mudi2}`);
-        mudi1 = mudi2;
-      } else {
-        // console.log("变化")
-        document.getElementById(mudi1).removeAttribute('style')
-        document.getElementById(mudi2).style['backgroundColor'] = "rgb(122, 255, 99)"
-        // console.log(`已经离开前一个div，前一个${mudi1},后一个${mudi2}`);
-        mudi1 = mudi2;
-      }
+        console.log(`不为空：target ID: ${targetID}`);
+        if (lastTargetID === targetID) {  // 判断一下两个ID是不是一个，也就是拖拽元素是不是还在同一个位置
+            console.log(`依旧留存在 ${targetID} 中`);
+        } else {
+            // 判断一下lastTargetID是不是空，在第一次移动的时候这个值为空
+            if (lastTargetID === undefined) {
+                // console.log(`已经离开前一个div，前一个${mudi1},后一个${mudi2}`);
+                lastTargetID = targetID;
+            } else {
+                console.log("变化")
+                document.getElementById(lastTargetID).removeAttribute('style')
+                lastTargetID = targetID;
+                document.getElementById(targetID).style['backgroundColor'] = "rgb(122, 255, 99)"
+                console.log(`已经离开前一个div，前一个${lastTargetID},目前处在：${targetID}`);
+
+            }
+        }
     }
-  }
 
 });
 //监听divList上的放下事件
+// TODO: 统一代码逻辑，Mac和Windows在dataTransfer中获取到的内容不一致
 divList.addEventListener("drop", async (event) => {
-  const c = event.dataTransfer.getData("text");
-  console.log(c)
-  if (c.search("文件") !== -1) {
     event.preventDefault();
     event.stopPropagation();
-    const selectType =
-      document.getElementById("select").options[
-        document.getElementById("select").selectedIndex
-      ].value;
-    const files = event.dataTransfer.files[0];
-    divList.innerHTML = await window.electronAPI.addFile(
-      [files.name, files.path],
-      selectType
-    );
-  } else {
-    const mudi = event.target.id;
-    if (mudi !== "") {
-    event.preventDefault();
-    event.stopPropagation();
+    const dropStartID = event.dataTransfer.getData("startID");
+    const file = event.dataTransfer.files[0];
+    if (file !== undefined) {
+        console.log(`dropStartID: ${dropStartID}`);
+        console.log(`file: ${file}`);
+        const selectType =
+            document.getElementById("select").options[
+                document.getElementById("select").selectedIndex
+                ].value;
+        console.log(`path:${file.path}`);
+        divList.innerHTML = await window.electronAPI.addFile(
+            [file.name, file.path],
+            selectType
+        );
+    } else {
+        console.log(`来源地id：${dropStartID}`);
+        console.log(`从over中获取的目的地：${lastTargetID}`)
+        document.getElementById(lastTargetID).removeAttribute('style')
     }
-    console.log(`来源地id：${c}`);
-    console.log(`目的地id：${mudi}`);
-  }
 });
