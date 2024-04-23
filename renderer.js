ce4344bdc72e0270d2423ae2102cd76def33e1e8
xiyiji <wujiaming@tsinghuavc.com.cn>
2024-04-24 00:31:51 +0800

    fix： css增加了列表中的margin。renderer增加了拖拽跟随移动的功能

--- renderer.js
+++ renderer.js
@@ -20,45 +20,58 @@ freshBtn.addEventListener("click", async () => {
     divList.innerHTML = await window.electronAPI.updateList(selectType);
 });
 
-
+let draging = null
 divList.addEventListener("dragstart", (event) => {
-    let targeElement = event.target.id;
+    const targeElement = event.target.id;
     console.log(targeElement)
     event.dataTransfer.setData("startID", targeElement);
+    event.dataTransfer.setData("html", event.target.innerHTML)
+    draging = event.target
+
 });
 
 // 监听divList上的【拖动到】事件
-var lastTargetID; // 设定一个变量用以接收ID的变化
+// :bug: 目前这个移动会溢出divlist，需要判断一下targetID的内容，不能是非空即可
 divList.addEventListener("dragover", (event) => {
     event.preventDefault();
     event.stopPropagation()
-    var targetID = event.target.id; // 目前停留在的目标元素ID
-
-    if (targetID === "") {
-        console.log(`为空：target ID: ${targetID}`); // 判断一下这个ID是不是存在
-    } else {
-        console.log(`不为空：target ID: ${targetID}`);
-        if (lastTargetID === targetID) {  // 判断一下两个ID是不是一个，也就是拖拽元素是不是还在同一个位置
-            console.log(`依旧留存在 ${targetID} 中`);
+    const targetID = event.target.id; // 目前停留在的目标元素ID
+    const dragingID = draging.id;
+    if (targetID !== "" && targetID !== dragingID) {
+        let targetRect = event.target.getBoundingClientRect();
+        let dragingRect = draging.getBoundingClientRect();
+        if (dragingID <targetID){
+            event.target.parentNode.insertBefore(draging, event.target.nextSibling);
         } else {
-            // 判断一下lastTargetID是不是空，在第一次移动的时候这个值为空
-            if (lastTargetID === undefined) {
-                // console.log(`已经离开前一个div，前一个${mudi1},后一个${mudi2}`);
-                lastTargetID = targetID;
-            } else {
-                console.log("变化")
-                document.getElementById(lastTargetID).removeAttribute('style')
-                lastTargetID = targetID;
-                document.getElementById(targetID).style['backgroundColor'] = "rgb(122, 255, 99)"
-                console.log(`已经离开前一个div，前一个${lastTargetID},目前处在：${targetID}`);
-
-            }
+            event.target.parentNode.insertBefore(draging, event.target);
         }
     }
+    /*
+        if (targetID === "") {
+            console.log(`为空：target ID: ${targetID}`); // 判断一下这个ID是不是存在
+        } else {
+            console.log(`不为空：target ID: ${targetID}`);
+            if (lastTargetID === targetID) {  // 判断一下两个ID是不是一个，也就是拖拽元素是不是还在同一个位置
+                console.log(`依旧留存在 ${targetID} 中`);
+            } else {
+                // 判断一下lastTargetID是不是空，在第一次移动的时候这个值为空
+                if (lastTargetID === undefined) {
+                    // console.log(`已经离开前一个div，前一个${mudi1},后一个${mudi2}`);
+                    lastTargetID = targetID;
+                } else {
+                    let targetRect = event.target.getBoundingClientRect();
+                    let dragingRect = draging.getBoundingClientRect();
+                    // console.log("变化")
+                    // document.getElementById(lastTargetID).removeAttribute('style')
+                    lastTargetID = targetID;
+                    // document.getElementById(targetID).style['backgroundColor'] = "rgb(122, 255, 99)"
+                    // console.log(`已经离开前一个div，前一个${lastTargetID},目前处在：${targetID}`);
+                }
+            }
+        }*/
 
 });
 //监听divList上的放下事件
-// TODO: 统一代码逻辑，Mac和Windows在dataTransfer中获取到的内容不一致
 divList.addEventListener("drop", async (event) => {
     event.preventDefault();
     event.stopPropagation();
@@ -78,7 +91,7 @@ divList.addEventListener("drop", async (event) => {
         );
     } else {
         console.log(`来源地id：${dropStartID}`);
-        console.log(`从over中获取的目的地：${lastTargetID}`)
-        document.getElementById(lastTargetID).removeAttribute('style')
+        console.log(`从over中获取的目的地：${draging.id}`)
+        // document.getElementById(lastTargetID).removeAttribute('style')
     }
 });
