893c1b237e2d0ef11d9a7668d405c50eefb2a3b8
xiyiji <wujiaming@tsinghauvc.com.cn>
2024-04-24 17:35:58 +0800

    :fix: 再次修改和优化了拖拽中获取参数和定位目标的逻辑

--- renderer.js
+++ renderer.js
@@ -2,6 +2,29 @@
 const freshBtn = document.getElementById("fresh");
 const divList = document.getElementById("list");
 
+
+function animate(prevRect, target) {
+    let ms = 500
+    if (ms) {
+        const currentRect = target.getBoundingClientRect()
+        if (prevRect.nodeType === 1) {
+            prevRect = prevRect.getBoundingClientRect()
+        }
+        target.style['transition'] = "none"
+        target.style['transform'] = `translate3d(${prevRect.left - currentRect.left}px, ${prevRect.top - currentRect.top}px, 0)`
+        target.offsetWidth;
+        target.style['transition'] = `all ${ms}ms`
+        target.style['transform'] = `translate3d(0, 0, 0)`;
+        clearTimeout(target.animated)
+        target.animated = setTimeout(() => {
+            target.removeAttribute('style')
+            target.animated = false
+        }, ms);
+
+    }
+}
+
+
 // 立即执行 初始化渲染中间的文件列表
 (async function () {
     const selectType =
@@ -24,7 +47,6 @@ let draging = null
 divList.addEventListener("dragstart", (event) => {
     const targeElement = event.target.id;
     console.log(targeElement)
-    event.dataTransfer.setData("startID", targeElement);
     event.dataTransfer.setData("html", event.target.innerHTML)
     draging = event.target
 
@@ -33,50 +55,57 @@ divList.addEventListener("dragstart", (event) => {
 // 监听divList上的【拖动到】事件
 // :bug: 目前这个移动会溢出divlist，需要判断一下targetID的内容，不能是非空即可
 divList.addEventListener("dragover", (event) => {
-    event.preventDefault();
-    event.stopPropagation()
     const targetID = event.target.id; // 目前停留在的目标元素ID
     const dragingID = draging.id;
-    if (targetID !== "" && targetID !== dragingID) {
-        let targetRect = event.target.getBoundingClientRect();
-        let dragingRect = draging.getBoundingClientRect();
-        if (dragingID <targetID){
-            event.target.parentNode.insertBefore(draging, event.target.nextSibling);
-        } else {
-            event.target.parentNode.insertBefore(draging, event.target);
-        }
-    }
-    /*
-        if (targetID === "") {
-            console.log(`为空：target ID: ${targetID}`); // 判断一下这个ID是不是存在
-        } else {
-            console.log(`不为空：target ID: ${targetID}`);
-            if (lastTargetID === targetID) {  // 判断一下两个ID是不是一个，也就是拖拽元素是不是还在同一个位置
-                console.log(`依旧留存在 ${targetID} 中`);
-            } else {
-                // 判断一下lastTargetID是不是空，在第一次移动的时候这个值为空
-                if (lastTargetID === undefined) {
-                    // console.log(`已经离开前一个div，前一个${mudi1},后一个${mudi2}`);
-                    lastTargetID = targetID;
-                } else {
-                    let targetRect = event.target.getBoundingClientRect();
-                    let dragingRect = draging.getBoundingClientRect();
-                    // console.log("变化")
-                    // document.getElementById(lastTargetID).removeAttribute('style')
-                    lastTargetID = targetID;
-                    // document.getElementById(targetID).style['backgroundColor'] = "rgb(122, 255, 99)"
-                    // console.log(`已经离开前一个div，前一个${lastTargetID},目前处在：${targetID}`);
-                }
-            }
-        }*/
+    const targetClass = event.target.className
+    console.log(event.target.className)
+    if ((targetClass === "list-text" || targetClass === "list-cell" || targetClass === "list-cell-warnning") ) {
+        console.log("处于可放置位置")
+        event.preventDefault();
+        event.stopPropagation()
 
+        if (targetClass === "list-text") {
+            let targetRect = event.target.parentNode.getBoundingClientRect();
+            let dragingRect = draging.getBoundingClientRect();
+            if (event.target.parentNode.animated) {
+                console.log("在同一个位置")
+            } else {
+                console.log("移动啦")
+                if (dragingID < targetID) {
+                    event.target.parentNode.parentNode.insertBefore(draging, event.target.parentNode.nextSibling);
+                } else {
+                    event.target.parentNode.parentNode.insertBefore(draging, event.target.parentNode);
+                }
+                animate(dragingRect, draging)
+                animate(targetRect, event.target.parentNode)
+            }
+        } else {
+            let targetRect = event.target.getBoundingClientRect();
+            let dragingRect = draging.getBoundingClientRect();
+            if (event.target.animated) {
+                console.log("在同一个位置")
+            } else {
+                console.log("移动啦")
+                if (dragingID < targetID) {
+                    event.target.parentNode.insertBefore(draging, event.target.nextSibling);
+                } else {
+                    event.target.parentNode.insertBefore(draging, event.target);
+                }
+                animate(dragingRect, draging)
+                animate(targetRect, event.target)
+            }
+        }
+
+    }
 });
+
 //监听divList上的放下事件
 divList.addEventListener("drop", async (event) => {
     event.preventDefault();
     event.stopPropagation();
     const dropStartID = event.dataTransfer.getData("startID");
     const file = event.dataTransfer.files[0];
+    console.log(`file: ${file}`);
     if (file !== undefined) {
         console.log(`dropStartID: ${dropStartID}`);
         console.log(`file: ${file}`);
@@ -90,8 +119,12 @@ divList.addEventListener("drop", async (event) => {
             selectType
         );
     } else {
-        console.log(`来源地id：${dropStartID}`);
-        console.log(`从over中获取的目的地：${draging.id}`)
-        // document.getElementById(lastTargetID).removeAttribute('style')
+        const dataList =  divList.getElementsByClassName("list-cell");
+        console.log(dataList)
+        for (let i = 0; i < dataList.length; i++) {
+            const fileName = dataList[i].getElementsByTagName("p")[0];
+            console.log(`fileName: ${fileName.innerText}`);
+        }
+
     }
 });
